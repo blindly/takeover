e3f322f92cfbb625d1ba07cfc6294af3b9b7f0b9
blindly <blindly@noreply.github.com>
2017-09-12 14:01:53 -0400

    Adding tabs

--- js/main.js
+++ js/main.js
@@ -99,6 +99,9 @@ var save = {
     toAtlantis: toAtlantis
 }
 
+chart();
+
+
 // Chart
 
 function chart() {
@@ -107,8 +110,13 @@ function chart() {
         labels: ['India', 'Africa', 'Asia', 'Europe', 'Americas', 'Atlantis'],
         type: 'pie'
     }];
+    
+    var layout = {
+      height: 300,
+      width: 400
+    };
 
-    Plotly.newPlot('chart', data);
+    Plotly.newPlot('chart', data, layout);
 }
 
 function coinClick(number) {
